dc0d588efb076296f0396b0f0bffb2b4d31a4579
Karel Soukup <karel.soukup@infomap.cz>
2019-06-21 22:17:10 +0200

    initial commit

---
+++ src/main/java/cz/soukup/lunde/LundeApplication.java
@@ -0,0 +1,13 @@
+package cz.soukup.lunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LundeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LundeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/soukup/lunde/LundeApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.soukup.lunde;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LundeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
